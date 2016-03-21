9d3cdb0c027a637a35e0ff24ff302d1b52793d73
mcablylx <308380504@qq.com>
2016-03-21 13:40:07 +0800

    onTouch和OnClick比对

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Android事件分发机制解析.
 
 在网上读了好几篇关于Android事件分发机制的文章.有写的很不错的.郭大神写的是尤为出色.<br>
 两篇文章:
- - [Android事件分发机制完全解析，带你从源码的角度彻底理解(上)](http://blog.csdn.net/guolin_blog/article/details/9097463),<br>
+ - [Android事件分发机制完全解析，带你从源码的角度彻底理解(上)](http://blog.csdn.net/guolin_blog/article/details/9097463)<br>
  - [Android事件分发机制完全解析，带你从源码的角度彻底理解(下)](http://blog.csdn.net/guolin_blog/article/details/9153747)
 
+根据郭大神的博客. 把栗子添里面. 方便观看.
 
-
-
-
+首先,我们给一个Button注册一个onClickListener  和  onTouchListener(示例:ActivityA)<br>
+运行后结果如下<br>
+<image src="./image/onClick_onTouch.png"/><br>
 
 
 
---
+++ app/src/main/java/com/demo/mckiera/eventdispatch/ActivityA.java
@@ -0,0 +1,38 @@
+package com.demo.mckiera.eventdispatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * author by Mckiera
+ * time: 2016/3/21  13:27
+ * description:
+ * updateTime:
+ * update description:
+ */
+public class ActivityA extends Activity {
+    private static final String TAG = "FUCK";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button btn = new Button(this);
+        setContentView(btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button onClick");
+            }
+        });
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i(TAG, "Button onTouch event.getAction() : " + event.getAction() );
+                return false;
+            }
+        });
+    }
+}
